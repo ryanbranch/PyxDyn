1cf3c32b8095372d28ef8ff0beb9350a77e56a7b
rjbranch <rjbrjb@umich.edu>
2016-02-01 00:14:48 -0500

    Fixed globals, and some other minor stuff
    The code no longer makes disgusting use of global variables, but instead
    employs a "Room" class to handle all of that.
    
    Also has support for argv, so the input file name can be given as the
    first argument
    
    test.csv has been renamed to input.csv, and a few subsequent changes as
    well (input_csv directory is now in gitignore and test_csv directory is
    gone)

--- play.py
+++ play.py
@@ -3,17 +3,113 @@ import csv
 import Image
 import os
 import errno
+import sys
 import _tkinter
 
 #Some global constants are defined here
 BACKGROUND_COLOR = (255, 255, 255)
 
-#Some global variables are defined here
-roomWidth = 0
-roomHeight = 0
-folderName = ""
-objects = []
-#root = _tkinter.Tk()
+#A class containing various variables central to the entire program
+class Room:
+    #MEMBER VARIABLES
+    # backgroundColor - the RGB value of the color of the window's background
+    # transparentColor - the RGB value to be treated as transparent
+    # roomWidth - the width of the window, in pixels
+    # roomHeight - the height of the window, in pixels
+    # fileName - the name of the file being used for simulation input
+    # folderName - the name of the folder in which to store data such as images
+    # objects - a list containing all of the objects
+
+    #Default constructor
+    def __init__(self):
+        self.backgroundColor = (255, 255, 255)
+        self.transparentColor = (255, 254, 253)
+        self.roomWidth = 1024
+        self.roomHeight = 768
+        self.fileName = "input.csv"
+        self.folderName = "input_csv"
+        self.objects = []
+
+    #Set function for backgroundColor
+    def setBackgroundColor(self, colorIn):
+        self.backgroundColor = colorIn
+
+    #Set function for transparentColor
+    def setTransparentColor(self, colorIn):
+        self.transparentColor = colorIn
+
+    #Set function for roomWidth
+    def setRoomWidth(self, widthIn):
+        self.roomWidth = widthIn
+
+    #Set function for roomHeight
+    def setRoomHeight(self, heightIn):
+        self.roomHeight = heightIn
+
+    #Set function for fileName
+    def setFileName(self, nameIn):
+        self.fileName = nameIn
+
+    #Set function for folderName
+    def setFolderName(self, nameIn):
+        self.folderName = nameIn
+
+    #Function to add an object to the end of "objects"
+    def addObject(self, objectIn):
+        self.objects.append(objectIn)
+
+    #Function to return the object at a given index of "objects"
+    def getObject(self, index):
+        return self.objects[index]
+
+    #Get function for backgroundColor
+    def getBackgroundColor(self):
+        return self.backgroundColor
+
+    #Get function for transparentColor
+    def getTransparentColor(self):
+        return self.transparentColor
+
+    #Get function for roomWidth
+    def getRoomWidth(self):
+        return self.roomWidth
+
+    #Get function for roomHeight
+    def getRoomWidth(self):
+        return self.roomHeight
+
+    #Get function for fileName
+    def getFileName(self):
+        return self.fileName
+
+    #Get function for folderName
+    def getFolderName(self):
+        return self.folderName
+
+    #Makes a directory with the name folderName, if one doesn't already exist
+    def makeFolder(self):
+        try:
+            os.makedirs(self.folderName)
+        except OSError as exception:
+            if exception.errno != errno.EEXIST:
+                raise
+
+    #Builds images of the objects in "objects" based on their appearance lists
+    def buildImages(self):
+        for obj in self.objects:
+            w = obj.getWidth()
+            h = obj.getHeight()
+            tempImage = Image.new('RGB', (w,h))
+            pixels = tempImage.load()
+            for y in range(h):
+                for x in range(w):
+                    pixels[x,y] = obj.getAppearance((y * w) + x)
+            tempImage.save(self.folderName +
+                           "/" +
+                           str(obj.getIdentifier()) +
+                           "_" +
+                           self.fileName +
+                           ".png")
 
 #A class describing any object that can be displayed on the screen
 class Object:
@@ -28,8 +124,8 @@ class Object:
         # isVisible - A boolean describing whether or not the object should be
         #             displayed visibly on the screen
         # image - an image representing the appearance list
-        # id - an integer representing the object.  Each object should
-        #      have a unique id, counting up from 1
+        # identifier - an integer representing the object.  Each object should
+        #              have a unique id, counting up from 1
 
     #Default constructor
     def __init__(self):
@@ -38,33 +134,20 @@ class Object:
         self.pos = 0.0
         self.appearance = []
         self.isVisible = True
-        self.id = 0
+        self.identifier = 0
 
     #Constructor Method
-    def __init__(self, width, height, pos, appearance, identifier):
+    def __init__(self, width, height, pos, appearance, id):
         self.width = width
         self.height = height
         self.pos = pos
         self.appearance = appearance
         self.isVisible = True
-        self.id = id
-
-    #Used to build an image of the object based on the appearance list
-    def buildImage(self):
-        print len(self.appearance)
-        for test in self.appearance:
-            print test
-        tempImage = Image.new('RGB', (self.width, self.height))
-        pixels = tempImage.load()
-        for y in range(self.height):
-            for x in range(self.width):
-                pixels[x,y] = self.appearance[(y * self.width) + x]
-        tempImage.save("test_csv" + "/test.png")
-        print("image saved.")
+        self.identifier = id
 
     #Get function for width
     def getWidth(self):
-		return self.width
+        return self.width
 
     #Get function for height
     def getHeight(self):
@@ -78,27 +161,18 @@ class Object:
     def getIsVisible(self):
         return self.isVisible
 
-def checkPath(path):
-    try:
-        os.makedirs(path)
-    except OSError as exception:
-        if exception.errno != errno.EEXIST:
-            raise
+    #Get function for identifier
+    def getIdentifier(self):
+        return self.identifier
 
-def fileInput():
-    #filename = raw_input('Enter filename, including extension.')
-    filename = "test.csv"
-    nameOfFolder = ""
-    for char in filename:
-        if (char == '.'):
-            nameOfFolder += "_"
-        else:
-            nameOfFolder += char
-    folderName = nameOfFolder
-    checkPath(folderName)
-    data = open(filename, 'rb')
+    #Function to return the color at a given index of "appearance"
+    def getAppearance(self, index):
+        return self.appearance[index]
+
+def fileInput(sim):
+    sim.makeFolder()
+    data = open(sim.getFileName(), 'rb')
     reader = csv.reader(data)
-    print ("unique message")
     rowNum = 0
     numObjects = 0
     objectIn = 0
@@ -114,8 +188,8 @@ def fileInput():
         print("row: " + str(rowNum))
         #Gets header data
         if (rowNum == 0):
-            roomWidth = int(row[0])
-            roomHeight = int(row[1])
+            sim.setRoomWidth(int(row[0]))
+            sim.setRoomHeight(int(row[1]))
             numObjects = int(row[2]) #Crash if this isn't 1 or more
             rowNum += 1
 
@@ -136,12 +210,11 @@ def fileInput():
                 else:
                     pixelsRemaining = -1
                     appearance = pixels[:]
-                    objects.append(Object(objWidth,
+                    sim.objects.append(Object(objWidth,
                                           objHeight,
                                           objPos,
                                           appearance,
                                           (objectIn + 1)))
-                    print("FINISHED MAKING OBJECT")
                     objectIn += 1
                     del pixels[:]
             rowNum += 1
@@ -155,15 +228,21 @@ def fileInput():
             return rowNum
 
 def playVideo(nextLine):
-    buildImages(objects)
-
-def buildImages(objList):
-    for obj in objList:
-        obj.buildImage()
-    objList[0].buildImage()
+    print"Video would be played here."
 
 def main():
-    nextLine = fileInput()
+    simulation = Room()
+    if (len(sys.argv) == 2):
+        simulation.setFileName(sys.argv[1])
+        tempName = ""
+        for char in sys.argv[1]:
+            if (char == '.'):
+                tempName += "_"
+            else:
+                tempName += char
+        simulation.setFolderName(tempName)
+    nextLine = fileInput(simulation)
+    simulation.buildImages()
     playVideo(nextLine)
     #window = _tkinter.Canvas(root, width = roomWidth, height = roomHeight)
 
